ad5737d78a13427329fe028fc67c156c263352fe
Alsey Coleman Miller <alseycmiller@gmail.com>
2015-03-27 21:14:43 -0500

    implemented CDAError

--- CDAFoundation/CDAFoundation/CDAError.h
+++ CDAFoundation/CDAFoundation/CDAError.h
@@ -7,7 +7,31 @@
 //
 
 #import <ObjFW/ObjFW.h>
+#import <CDAFoundation/CDAInteger.h>
 
+extern OFString * const CDAErrorDescriptionKey;
+
+/** Used to describe a non-fatal error. */
 @interface CDAError : OFObject
+
+#pragma mark - Intialization
+
++ (instancetype)errorWithDomain:(OFString *)domain
+                           code:(CDAInteger)code
+                       userInfo:(OFDictionary *)userInfo;
+
+- (instancetype)initWithDomain:(OFString *)domain
+                          code:(CDAInteger)code
+                      userInfo:(OFDictionary *)userInfo;
+
+#pragma mark - Properties
+
+@property (readonly) CDAInteger code;
+
+@property (readonly, copy) OFString *domain;
+
+@property (readonly, copy) OFDictionary *userInfo;
+
+@property (readonly, copy) OFString *description;
 
 @end
--- CDAFoundation/CDAFoundation/CDAError.m
+++ CDAFoundation/CDAFoundation/CDAError.m
@@ -8,6 +8,48 @@
 
 #import "CDAError.h"
 
+OFString * const CDAErrorDescriptionKey = @"CDAErrorDescriptionKey";
+
+@interface CDAError ()
+
+@property CDAInteger code;
+
+@property (copy) OFString *domain;
+
+@property (copy) OFDictionary *userInfo;
+
+@property (copy) OFString *description;
+
+@end
+
 @implementation CDAError
+
++ (instancetype)errorWithDomain:(OFString *)domain
+                           code:(CDAInteger)code
+                       userInfo:(OFDictionary *)userInfo
+{
+    return [[self alloc] initWithDomain:domain
+                            code:code
+                        userInfo:userInfo];
+}
+
+- (instancetype)initWithDomain:(OFString *)domain
+                          code:(CDAInteger)code
+                      userInfo:(OFDictionary *)userInfo
+{
+    self = [super init];
+    if (self) {
+        
+        // required
+        self.domain = domain;
+        self.code = code;
+        self.userInfo = userInfo;
+        
+        // optional
+        self.description = userInfo[CDAErrorDescriptionKey];
+        
+    }
+    return self;
+}
 
 @end
